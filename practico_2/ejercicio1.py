c4794330e1ba990641f1011fc521f04858518c8e
matias-dogliani <matiasdogliani@hotmail.com>
2021-03-24 20:22:41 -0300

    Update

--- practico_2/ejercicio1.py
+++ practico_2/ejercicio1.py
@@ -1,23 +1,30 @@
-#from cv2 import imread, imwrite
 import cv2 
 
-imagen = cv2.imread("./hoja.png") 
-UMBRAL = 240 
-""" otro método es convertir la imagen a un solo canal en 
-    escala de grises. 
 
-    img_gris = imagen[:,:,0]+ imagen[:,:,1] + imagen[:,:,2] 
+""" Puedo importar con 1 solo canal o con 3 canales y 
+pasarlo a pata a escalas de grises 
+
+img_gris = imagen[:,:,0]+ imagen[:,:,1] + imagen[:,:,2]
 """ 
 
-for i in range(imagen.shape[2]):      #3 Canales 
-    for j in range(imagen.shape[1]): 
-        for k in range(imagen.shape[0]): 
-            if (imagen[k,j,i] < UMBRAL): 
-                print( imagen[k,j,i])  
-                imagen[k,j,i] =0;
-            else: 
-                imagen[k,j,i] = 255; 
+imagen = cv2.imread("./hoja.png",0) 
+img_orig = imagen
 
-"""Binarizacion? ----> Si es mayor Negro, si es menor Blanco""" 
 
-cv2.imwrite("resultado.png", imagen) 
+UMBRAL = 240 
+
+cv2.namedWindow("Original")
+cv2.imshow("Original",img_orig)
+
+
+for i in range(imagen.shape[0]): 
+    for j in range(imagen.shape[1]):
+        if (imagen[i,j] < UMBRAL): 
+            imagen[i,j]= 0 
+        else: 
+            imagen[i,j]= 255
+
+cv2.namedWindow("Resultado")
+cv2.imshow("Resultado",imagen)
+cv2.waitKey() 
+cv2.imwrite("resultado.png",imagen) 
